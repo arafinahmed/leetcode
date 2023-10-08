8a0e1e7b71089ae525fecf18db15ad611d4d455f
Arafin Ahmed <arafin2021@gmail.com>
2023-10-08 13:19:55 +0600

    better solution :

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -5,11 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for num in nums:
-            remainings = target - num
-            if remainings in nums:
-                if remainings == num:
-                    if nums.count(num) > 1:
-                        return [i for i, x in enumerate(nums) if x == num]
-                else:
-                    return [nums.index(num), nums.index(remainings)]
+        num_map = {}
+        l = len(nums)
+        for i in range(l):
+            num_map[nums[i]] = i
+
+        for i in range(l):
+            remaining = target - nums[i]
+            if remaining in num_map and num_map[remaining] != i:
+                return [i, num_map[remaining]]
+        
+        return []
