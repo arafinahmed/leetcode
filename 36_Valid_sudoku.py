28e59a8251740d20a987fdbce01d9aac5c270a37
Arafin Ahmed <arafin2021@gmail.com>
2023-10-10 10:15:25 +0600

    36

--- 36_Valid_sudoku.py
+++ 36_Valid_sudoku.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for row in board:
-            for col in row:
-                print(col)
+        row_dict = [{} for _ in range(9)]
+        col_dict = [{} for _ in range(9)]
+        region_dict = [{} for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] in row_dict[i]:
+                        return False
+                    else:
+                        row_dict[i][board[i][j]] = 1
+                    
+                    if board[i][j] in col_dict[j]:
+                        return False
+                    else:
+                        col_dict[j][board[i][j]] = 1
+
+                    if board[i][j] in region_dict[i//3+(j//3)*3]:
+                        return False
+                    else:
+                        region_dict[i//3+(j//3)*3][board[i][j]] = 1
+        return True
