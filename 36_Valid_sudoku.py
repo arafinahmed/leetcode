97e73ef1e822eca8e0c5a3c5b7ab5275832aaaa8
Arafin Ahmed <arafin2021@gmail.com>
2023-10-10 10:15:00 +0600

    36

---
+++ 36_Valid_sudoku.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for row in board:
+            for col in row:
+                print(col)
