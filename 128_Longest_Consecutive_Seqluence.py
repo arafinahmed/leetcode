4a8395c35f22ff20bff4858ec38a387cf941b1a3
Arafin Ahmed <arafin2021@gmail.com>
2023-10-11 10:35:09 +0600

    worse then prev

--- 128_Longest_Consecutive_Seqluence.py
+++ 128_Longest_Consecutive_Seqluence.py
@@ -1,16 +1,36 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        best = 0
+
+        for num in nums:
+            if num - 1 not in nums:
+                y = num + 1
+                while y in nums:
+                    y += 1
+                best = max(best, y - num)
+        return best
+
 # class Solution(object):
 #     def longestConsecutive(self, nums):
 #         """
 #         :type nums: List[int]
 #         :rtype: int
 #         """
-#         nums = set(nums)
+#         nums_dict = {}
 #         best = 0
 
 #         for num in nums:
-#             if num - 1 not in nums:
-#                 y = num + 1
-#                 while y in nums:
+#             nums_dict[num] = 1
+        
+#         for key in nums_dict:
+#             if key - 1 not in nums_dict:
+#                 y = key + 1
+#                 while y in nums_dict:
 #                     y += 1
-#                 best = max(best, y - num)
+#                 best = max(best, y - key)
 #         return best
