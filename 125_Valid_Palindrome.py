2cd75cdd5a71c88985341419b6ee1b582723d6a5
Arafin Ahmed <arafin2021@gmail.com>
2023-10-12 10:52:52 +0600

    valid palindrome better

--- 125_Valid_Palindrome.py
+++ 125_Valid_Palindrome.py
@@ -1,3 +1,20 @@
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         # s = s.lower()
+#         main_str = ''
+#         for char in s:
+#             if char.isalnum():
+#                 main_str += char.lower()
+        
+#         print(main_str)
+#         print(main_str[::-1])
+#         return main_str == main_str[::-1]
+
+
 class Solution(object):
     def isPalindrome(self, s):
         """
@@ -5,11 +22,13 @@ class Solution(object):
         :rtype: bool
         """
         # s = s.lower()
-        main_str = ''
-        for char in s:
-            if char.isalnum():
-                s = main_str + char.lower()
-        
-        print(main_str)
-        print(main_str[::-1])
-        return main_str == main_str[::-1]
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum(): 
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1; r -= 1
+        return True
