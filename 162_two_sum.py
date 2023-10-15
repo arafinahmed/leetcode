8e5be8e957b25b8471880cf8b538031c3ccec16e
Arafin Ahmed <arafin2021@gmail.com>
2023-10-15 09:00:51 +0600

    two sum sorted

--- 162_two_sum.py
+++ 162_two_sum.py
@@ -1,3 +1,19 @@
+# class Solution(object):
+#     def twoSum(self, numbers, target):
+#         """
+#         :type numbers: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         l, r = 0, len(numbers)-1
+#         while l < r:
+#             if numbers[l] + numbers[r] == target:
+#                 return [l+1, r+1]
+#             elif numbers[l] + numbers[r] > target:
+#                 r -= 1
+#             else:
+#                 l += 1
+
 class Solution(object):
     def twoSum(self, numbers, target):
         """
@@ -5,11 +21,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        l, r = 0, len(numbers)-1
-        while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            else:
-                l += 1
+        dic = {}
+
+        for i, num in enumerate(numbers):
+            if target - num in dic:
+                return [dic[target - num] + 1, i + 1]
+            dic[num] = i
+
