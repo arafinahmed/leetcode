c07fd97b23a23d07f8e03c94edc9827cbb6826fa
Arafin Ahmed <arafin2021@gmail.com>
2023-10-22 16:13:39 +0600

    longest

--- 424_Longest.py
+++ 424_Longest.py
@@ -5,7 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        print(ord('A'))
+        dict_letter = {}
+        for i in range(ord('A'), ord('Z') + 1):
+            dict_letter[chr(i)] = 0
+        
+        n = len(s)
+        max_len = 0
+        maxCharCount = 0
+        start = 0
+
+        for end in range(n):
+            dict_letter[s[end]] += 1
+            maxCharCount = max(maxCharCount, dict_letter[s[end]])
+            
+            if end - start + 1 - maxCharCount > k:
+                dict_letter[s[start]] -= 1
+                start += 1
+            
+            max_len = max(max_len, end - start + 1)
+        return max_len
+
+        
 
 
 sol = Solution()
