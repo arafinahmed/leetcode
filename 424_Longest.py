30abfc4063bb2f9544a3acadde807044e869360d
Arafin Ahmed <arafin2021@gmail.com>
2023-10-22 16:09:11 +0600

    longest

---
+++ 424_Longest.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        print(ord('A'))
+
+
+sol = Solution()
+print(sol.characterReplacement("ABAB", 2))
