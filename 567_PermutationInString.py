29adbce91a12f0e88e82052fd3598af69cf12c78
Arafin Ahmed <arafin2021@gmail.com>
2023-10-23 10:18:25 +0600

    not optimized

--- 567_PermutationInString.py
+++ 567_PermutationInString.py
@@ -1,20 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         from collections import Counter
-        counter, w = Counter(s1), len(s1)
+        counter, w, match = Counter(s1), len(s1), 0
         
         len_s2 = len(s2)
         
         for i in range(len_s2):
             if s2[i] in counter:
+                if not counter[s2[i]]:
+                    match -= 1
                 counter[s2[i]] -= 1
+                if not counter[s2[i]]:
+                    match += 1
             
             if i >= w and s2[i-w] in counter:
+                if not counter[s2[i-w]]:
+                    match -= 1
                 counter[s2[i-w]] += 1
+                if not counter[s2[i-w]]:
+                    match += 1
 
-            if all(val ==0 for val in counter.values()):
+            if match == len(counter):
                 return True
-            
         return False
 
 sol = Solution()
