38aadd18ab72fb7d212efcfe082e94c902c72b71
Arafin Ahmed <arafin2021@gmail.com>
2023-11-08 10:24:41 +0600

    next

---
+++ 287_Find_Duplicate.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[abs(nums[i])] < 0:
+                return abs(nums[i])
+            else:
+                nums[abs(nums[i])] *= -1
+
+        
