63c2e5ea84bb7e54aa99a5950bd03bb80ede17a7
Arafin Ahmed <arafin2021@gmail.com>
2023-11-08 09:25:49 +0600

    Fast sol

---
+++ 141_LinkedList.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         dict = {}
+
+#         while head:
+#             if head.next in dict:
+#                 return True
+#             else:
+#                 dict[head.next] = 1
+#             head = head.next
+
+#         return False
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        if not head: return False
+
+        slow = fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
