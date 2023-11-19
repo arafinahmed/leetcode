eb64f85b94be322cb52d190951630fde9263762d
Arafin Ahmed <arafin2021@gmail.com>
2023-11-19 09:59:18 +0600

    right view

---
+++ 199_Binary_Tree.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        queue = [root]
+
+        while queue:
+            level = []
+            l = len(queue)
+
+            for _ in range(l):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.append(level[-1])
+        
+
+        return res
