8158f9ab733335eed272efb5e6876c507cabb96d
Arafin Ahmed <arafin2021@gmail.com>
2023-12-03 09:45:52 +0600

    slow

---
+++ december/17_letter_Combination.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        if not digits:
+            return []
+        
+        res = []
+        self.dfs(digits, 0, [], res)
+        return res
+    
+    def dfs(self, digits, index, path, res):
+        if index == len(digits):
+            res.append(''.join(path))
+            return
+        
+        for c in self.getLetters(digits[index]):
+            path.append(c)
+            self.dfs(digits, index + 1, path, res)
+            path.pop()
+
+    def getLetters(self, digit):
+        if digit == '2':
+            return 'abc'
+        elif digit == '3':
+            return 'def'
+        elif digit == '4':
+            return 'ghi'
+        elif digit == '5':
+            return 'jkl'
+        elif digit == '6':
+            return 'mno'
+        elif digit == '7':
+            return 'pqrs'
+        elif digit == '8':
+            return 'tuv'
+        elif digit == '9':
+            return 'wxyz'
+        else:
+            return ''
