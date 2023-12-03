028be825dd27ee3cce6c499af1264cb55c365a25
Arafin Ahmed <arafin2021@gmail.com>
2023-12-03 09:38:23 +0600

    palindrome

---
+++ december/131_Palindrome.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if not s:
+            return [[]]
+        res = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:
+                for rest in self.partition(s[i:]):
+                    res.append([s[:i]] + rest)
+        return res
+
+        
+        
