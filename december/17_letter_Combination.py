be80af4ab1ac7df53341c0ef610747823531f267
Arafin Ahmed <arafin2021@gmail.com>
2023-12-03 09:57:31 +0600

    letter

--- december/17_letter_Combination.py
+++ december/17_letter_Combination.py
@@ -1,41 +1,65 @@
 from typing import List
 
+# class Solution:
+#     def letterCombinations(self, digits: str) -> List[str]:
+        
+#         if not digits:
+#             return []
+        
+#         res = []
+#         self.dfs(digits, 0, [], res)
+#         return res
+    
+#     def dfs(self, digits, index, path, res):
+#         if index == len(digits):
+#             res.append(''.join(path))
+#             return
+        
+#         for c in self.getLetters(digits[index]):
+#             path.append(c)
+#             self.dfs(digits, index + 1, path, res)
+#             path.pop()
+
+#     def getLetters(self, digit):
+#         if digit == '2':
+#             return 'abc'
+#         elif digit == '3':
+#             return 'def'
+#         elif digit == '4':
+#             return 'ghi'
+#         elif digit == '5':
+#             return 'jkl'
+#         elif digit == '6':
+#             return 'mno'
+#         elif digit == '7':
+#             return 'pqrs'
+#         elif digit == '8':
+#             return 'tuv'
+#         elif digit == '9':
+#             return 'wxyz'
+#         else:
+#             return ''
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        if not digits:
+        mapping = {
+            '2': 'abc', 
+            '3': 'def', 
+            '4': 'ghi', 
+            '5': 'jkl', 
+            '6': 'mno', 
+            '7': 'pqrs', 
+            '8': 'tuv', 
+            '9': 'wxyz'
+        }
+
+        if len(digits) == 0:
             return []
         
-        res = []
-        self.dfs(digits, 0, [], res)
-        return res
-    
-    def dfs(self, digits, index, path, res):
-        if index == len(digits):
-            res.append(''.join(path))
-            return
+        if len(digits) == 1:
+            return list(mapping[digits[0]])
         
-        for c in self.getLetters(digits[index]):
-            path.append(c)
-            self.dfs(digits, index + 1, path, res)
-            path.pop()
+        prev = self.letterCombinations(digits[:-1])
+        additional = mapping[digits[-1]]
 
-    def getLetters(self, digit):
-        if digit == '2':
-            return 'abc'
-        elif digit == '3':
-            return 'def'
-        elif digit == '4':
-            return 'ghi'
-        elif digit == '5':
-            return 'jkl'
-        elif digit == '6':
-            return 'mno'
-        elif digit == '7':
-            return 'pqrs'
-        elif digit == '8':
-            return 'tuv'
-        elif digit == '9':
-            return 'wxyz'
-        else:
-            return ''
+        return [s + c for s in prev for c in additional]
