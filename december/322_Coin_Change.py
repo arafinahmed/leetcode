7e057bd987770d4ee6caf81f86a4790800ece779
Arafin Ahmed <arafin2021@gmail.com>
2023-12-18 10:20:15 +0600

    dp

--- december/322_Coin_Change.py
+++ december/322_Coin_Change.py
@@ -2,15 +2,11 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1] * (amount+1)
-        dp[0] = 0
+        dp = [0] + [amount+1] * amount
 
-        for i in range(1, amount+1):
-            for c in coins:
-                if i>=c:
-                    dp[i] = min(dp[i], dp[i-c]+1)
 
-        if dp[amount] == amount+1:
-            return -1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
         
-        return dp[amount]
+        return -1 if dp[amount] == amount + 1 else dp[amount]
