83cd07a070f77fe5371de48bfdd2464da4b87717
Arafin Ahmed <arafin2021@gmail.com>
2024-01-18 10:08:59 +0600

    done

---
+++ january/53_Maximum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currentSum = nums[0]
+
+        for num in nums[1:]:
+            currentSum = max(num, currentSum + num)
+            maxSum = max(currentSum, maxSum)
+
+        return maxSum
